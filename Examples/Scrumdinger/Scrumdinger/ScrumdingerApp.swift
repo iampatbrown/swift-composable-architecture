44d0f62512a2987a7935f40a1b391d6117e5d598
Pat Brown <mrpatbrown@gmail.com>
2021-09-22 14:23:37 +1000

    Initial setup

---
+++ Examples/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Pat Brown on 22/9/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
