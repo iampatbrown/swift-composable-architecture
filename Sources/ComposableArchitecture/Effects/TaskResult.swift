af5cc96beb2c41e9ce7f3247200f092a2a482939
Pat Brown <mrpatbrown@gmail.com>
2022-10-15 11:23:52 +1100

    Remove Success equatable requirement

--- Sources/ComposableArchitecture/Effects/TaskResult.swift
+++ Sources/ComposableArchitecture/Effects/TaskResult.swift
@@ -205,33 +205,39 @@ enum TaskResultDebugging {
   @TaskLocal static var emitRuntimeWarnings = true
 }
 
-extension TaskResult: Equatable where Success: Equatable {
+extension TaskResult: Equatable {
   public static func == (lhs: Self, rhs: Self) -> Bool {
-    switch (lhs, rhs) {
-    case let (.success(lhs), .success(rhs)):
-      return lhs == rhs
-    case let (.failure(lhs), .failure(rhs)):
-      return _isEqual(lhs, rhs) ?? {
+    func isEqual(_ lhs: Any, _ rhs: Any) -> Bool {
+      if let isEqual = _isEqual(lhs, rhs) {
+        return isEqual
+      } else {
         #if DEBUG
-          let lhsType = type(of: lhs)
-          if TaskResultDebugging.emitRuntimeWarnings, lhsType == type(of: rhs) {
-            let lhsTypeName = typeName(lhsType)
-            runtimeWarn(
-              """
-              "\(lhsTypeName)" is not equatable. …
+        let lhsType = type(of: lhs)
+        if TaskResultDebugging.emitRuntimeWarnings, lhsType == type(of: rhs) {
+          let lhsTypeName = typeName(lhsType)
+          runtimeWarn(
+            """
+            "\(lhsTypeName)" is not equatable. …
 
-              To test two values of this type, it must conform to the "Equatable" protocol. For \
-              example:
+            To test two values of this type, it must conform to the "Equatable" protocol. For \
+            example:
 
-                  extension \(lhsTypeName): Equatable {}
+                extension \(lhsTypeName): Equatable {}
 
-              See the documentation of "TaskResult" for more information.
-              """
-            )
-          }
+            See the documentation of "TaskResult" for more information.
+            """
+          )
+        }
         #endif
         return false
-      }()
+      }
+    }
+      
+    switch (lhs, rhs) {
+    case let (.success(lhs), .success(rhs)):
+      return isEqual(lhs, rhs)
+    case let (.failure(lhs), .failure(rhs)):
+      return isEqual(lhs, rhs)
     default:
       return false
     }
